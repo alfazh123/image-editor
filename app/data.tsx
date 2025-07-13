1294a6e40e654890bd5a0b6ab17a83debbf18d5e
alfazh123 <alfazh291@gmail.com>
2025-07-13 22:08:20 +0700

    add menu filter props

---
+++ app/data.tsx
@@ -0,0 +1,57 @@
+export const filterMenuItems = [
+    {
+        label: 'Cartoon',
+        imgUrl: '/img_filter/cartoon.jpeg',
+        backgroundColor: 'bg-red-400',
+    },
+    {
+        label: 'Dart Vader',
+        imgUrl: '/img_filter/darth-vader.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Fight Club',
+        imgUrl: '/img_filter/fight-club.jpg',
+        backgroundColor: 'bg-green-400',
+    },
+    {
+        label: 'Ocean',
+        imgUrl: '/img_filter/ocean.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Paint 1',
+        imgUrl: '/img_filter/paint-1.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Paint 2',
+        imgUrl: '/img_filter/paint-2.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Stormtrooper',
+        imgUrl: '/img_filter/stormtrooper.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Superman',
+        imgUrl: '/img_filter/superman.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'The Bear',
+        imgUrl: '/img_filter/the-bear.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'Toy Story',
+        imgUrl: '/img_filter/toy-story-1.jpg',
+        backgroundColor: 'bg-blue-400',
+    },
+    {
+        label: 'You are the Apple',
+        imgUrl: '/img_filter/you-are-the-apple-of-my-eye.jpg',
+        backgroundColor: 'bg-blue-400',
+    }
+]
