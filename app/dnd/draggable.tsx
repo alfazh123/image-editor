16f8d05b7fb373f213158b24835c32189835c293
alfazh123 <alfazh291@gmail.com>
2025-07-23 22:57:05 +0700

    fix overflow action draggable

--- app/dnd/draggable.tsx
+++ app/dnd/draggable.tsx
@@ -2,59 +2,59 @@ import React from 'react';
 import {useDraggable} from '@dnd-kit/core';
 
 interface DraggableProps {
-    id: string;
-    children: React.ReactNode;
-    isAvailable?: boolean;
-    windowSize: { width: number; height: number };
-    position?: { x: number; y: number };
+	id: string;
+	children: React.ReactNode;
+	isAvailable?: boolean;
+	windowSize: { width: number; height: number };
+	position?: { x: number; y: number };
 }
 
-export function Draggable({ id, children, isAvailable, windowSize, position = { x: 0, y: 0 } }: DraggableProps): React.ReactElement {
+export function Draggable({
+	id,
+	children,
+	isAvailable,
+	windowSize,
+	position = { x: 0, y: 0 },
+}: DraggableProps): React.ReactElement {
 	const { attributes, listeners, setNodeRef, transform, isDragging } =
 		useDraggable({
 			id,
 		});
 
+	const draggableItem = document.getElementById(id);
+	const draggableWidth = draggableItem?.clientWidth || 0;
+	const draggableHeight = draggableItem?.clientHeight || 0;
+
 	position = {
 		x:
-			position.x + 20 > windowSize.width
-				? windowSize.width - 10
-				: position.x + 300 < 0
-				? -100
+			position.x > windowSize.width
+				? windowSize.width + 150
+				: position.x + draggableWidth < 10
+				? position.x + 100
 				: position.x,
 		y:
-			position.y + 20 > windowSize.height
-				? windowSize.height - 10
-				: position.y + 100 < 0
-				? -10
+			position.y > windowSize.height
+				? windowSize.height - 150
+				: position.y + draggableHeight < 10
+				? position.y + 100
 				: position.y,
 	};
 
-	let style: React.CSSProperties;
-	if (!isAvailable) {
-		style = {
-			position: "absolute",
-			left: position.x,
-			top: position.y,
-			transform: transform
-				? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-				: undefined,
-			zIndex: isDragging ? 1000 : 1,
-		};
-	} else {
-		style = {
-			position: "relative",
-			left: position.x,
-			top: position.y,
-			transform: transform
-				? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-				: undefined,
-			zIndex: isDragging ? 1000 : 1,
-		};
-	}
+	console.log(windowSize.width);
+
+	const style: React.CSSProperties = {
+		position: `${isAvailable ? "relative" : "absolute"}`,
+		left: position.x,
+		top: position.y,
+		transform: transform
+			? `translate3d(${transform.x}px, ${transform.y}px, 0)`
+			: undefined,
+		zIndex: 1,
+	};
 
 	return (
 		<div
+			id={id}
 			ref={setNodeRef}
 			style={{
 				...style,
@@ -65,9 +65,9 @@ export function Draggable({ id, children, isAvailable, windowSize, position = { 
 			{...attributes}
 			className={`
             flex items-center justify-center relative rounded-lg font-medium text-slate-500
-            bg-white from shadow-lg hover:shadow-xl active:scale-95
+            bg-transparent backdrop-blur-lg from shadow-lg hover:shadow-xl active:scale-95
             cursor-grab active:cursor-grabbing select-none
-			${isDragging ? "opacity-80 scale-110 rotate-2" : "hover:scale-105"}
+			${isDragging ? "scale-110 rotate-2" : "hover:scale-105"}
             ${isAvailable ? "" : "w-96 h-56 px-6 py-3 border border-slate-100"}
             `}>
 			{children}
--- app/hooks/useImageEditor.tsx
+++ app/hooks/useImageEditor.tsx
@@ -9,34 +9,43 @@ export const useImageEditor = () => {
     const [imgRefUrl, setImgRefUrl] = useState<string | null>(null);
     const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
     const [isLoading, setIsLoading] = useState(false);
-    
-    const [sharpVal, setSharpVal] = useState(0);
-    const [lightVal, setLightVal] = useState<LightValueProps>({
-        exposureValue: 0,
-        contrastValue: 0,
-    });
-    const [colorVal, setColorVal] = useState<ColorValueProps>({
-        saturationValue: 0,
-        temperatureValue: 0,
-        tintValue: 0,
-    });
 
-    const ArrToURL = useCallback((arr: Uint8Array): string => {
-        return URL.createObjectURL(new Blob([arr], { type: "image/png" }));
-    }, []);
+	const [sharpVal, setSharpVal] = useState(0);
+	const [lightVal, setLightVal] = useState<LightValueProps>({
+		exposureValue: 0,
+		contrastValue: 0,
+	});
+	const [colorVal, setColorVal] = useState<ColorValueProps>({
+		saturationValue: 0,
+		temperatureValue: 0,
+		tintValue: 0,
+	});
 
-    return {
-        // States
-        originalImgArr, setOriginalImgArr,
-        refImgArr, setRefImgArr,
-        imgUrl, setImgUrl,
-        imgRefUrl, setImgRefUrl,
-        imageSize, setImageSize,
-        isLoading, setIsLoading,
-        sharpVal, setSharpVal,
-        lightVal, setLightVal,
-        colorVal, setColorVal,
-        // Utils
-        ArrToURL
-    };
+	const ArrToURL = useCallback((arr: Uint8Array): string => {
+		return URL.createObjectURL(new Blob([arr], { type: "image/png" }));
+	}, []);
+
+	return {
+		// States
+		originalImgArr,
+		setOriginalImgArr,
+		refImgArr,
+		setRefImgArr,
+		imgUrl,
+		setImgUrl,
+		imgRefUrl,
+		setImgRefUrl,
+		imageSize,
+		setImageSize,
+		isLoading,
+		setIsLoading,
+		sharpVal,
+		setSharpVal,
+		lightVal,
+		setLightVal,
+		colorVal,
+		setColorVal,
+		// Utils
+		ArrToURL,
+	};
 };
