7bc83900c10c5ab6899376ab27042ce9e5e50821
alfazh123 <alfazh291@gmail.com>
2025-08-01 22:56:10 +0700

    add more component from shadcn

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -151,16 +151,13 @@ function DropdownMenuLabel({
   inset?: boolean
 }) {
   return (
-    <DropdownMenuPrimitive.Label
-      data-slot="dropdown-menu-label"
-      data-inset={inset}
-      className={cn(
-        "px-2 py-1.5 text-sm font-medium data-[inset]:pl-8",
-        className
-      )}
-      {...props}
-    />
-  )
+		<DropdownMenuPrimitive.Label
+			data-slot="dropdown-menu-label"
+			data-inset={inset}
+			className={cn("py-1.5 text-sm font-medium data-[inset]:pl-8", className)}
+			{...props}
+		/>
+	);
 }
 
 function DropdownMenuSeparator({
