f58c5560fd495a392d175819951439d90097bd3e
alfazh123 <alfazh291@gmail.com>
2026-01-11 23:51:31 +0700

    revamp some section in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -6,54 +6,96 @@ import Link from "next/link";
 import FeatureCard from "@/components/feature-card";
 import Navbar from "@/components/navbar";
 import { Github } from "lucide-react";
-
-const features = [
-	{
-		title: "Image Color Transfer",
-		demo: "feature-demo/transfer-color.mp4",
-		icon: "feature-icon/one.svg",
-		backgroundColor: "bg-[#6CBBFB]",
-	},
-	{
-		title: "Adjust Sharpness",
-		demo: "feature-demo/sharp.mp4",
-		icon: "feature-icon/two.svg",
-		backgroundColor: "bg-[#69DB7C]",
-	},
-	{
-		title: "Adjust Lightness",
-		demo: "feature-demo/light.mp4",
-		icon: "feature-icon/three.svg",
-		backgroundColor: "bg-[#ff8787]",
-	},
-	{
-		title: "Adjust Color Image",
-		demo: "feature-demo/color.mp4",
-		icon: "feature-icon/four.svg",
-		backgroundColor: "bg-[#FFE792]",
-	},
-];
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-	console.log("tes");
+	const [imgIndex, setImgIndex] = useState(0);
+	const [imageSrc, setImageSrc] = useState("/animation/tools-1.svg");
+	const header = useRef<HTMLDivElement>(null);
+	const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+	useEffect(() => {
+		if (!header.current) return;
+
+		const handleMouseEnter = () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			intervalRef.current = setInterval(() => {
+				setImgIndex((prev) => {
+					const nextIndex = prev + 1;
+					if (nextIndex > 21) {
+						return 21;
+					}
+					setImageSrc(`/animation/tools-${nextIndex}.svg`);
+					return nextIndex;
+				});
+			}, 40);
+		};
+
+		const handleMpuseLeave = () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			intervalRef.current = setInterval(() => {
+				setImgIndex((prev) => {
+					const nextIndex = prev - 1;
+					if (nextIndex >= 1) {
+						setImageSrc(`/animation/tools-${nextIndex}.svg`);
+						return nextIndex;
+					} else if (prev <= 0) {
+						setImageSrc(`/animation/tools-1.svg`);
+						return 1;
+					}
+					return nextIndex;
+				});
+			}, 40);
+		};
+		header.current.addEventListener("mouseenter", handleMouseEnter);
+		header.current.addEventListener("mouseleave", handleMpuseLeave);
+
+		return () => {
+			if (!header.current) return;
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+			header.current.addEventListener("mouseenter", handleMouseEnter);
+			header.current.addEventListener("mouseleave", handleMpuseLeave);
+		};
+	}, []);
+
 	return (
 		<div>
 			<div className="flex flex-col xl:w-1/2 sm:w-3/4 w-full mx-auto sm:px-8 px-4">
 				<Navbar />
-				<header className="flex flex-col w-full h-80 justify-center relative mb-10">
+				<header
+					ref={header}
+					className="flex flex-col w-full h-80 justify-center relative mb-10">
 					<div className="flex flex-col gap-4 lg:max-w-1/2">
 						<h1 className="md:text-6xl text-4xl font-bold">
 							Image editor with WASM
 						</h1>
 					</div>
 					<Image
-						src={"tools.svg"}
+						src={imageSrc}
 						alt="app screenshot"
 						width={800}
 						height={600}
-						className="w-96 rounded-xl p-1 lg:block hidden absolute -right-20 top-10 mask-r-from-40% mask-r-to-90%"
+						className="w-96 rounded-xl -rotate-12 p-1 lg:block hidden absolute -right-20 top-10 mask-r-from-40% mask-r-to-90%"
 					/>
 				</header>
+
+				{/* Feature */}
+				<div className="flex lg:flex-row flex-col gap-4 mt-12 items-center">
+					<div>
+						<h2 className="text-4xl font-semibold">Features</h2>
+						<p>
+							Explore the various features of this image editor, powered by Rust
+							and WebAssembly.
+						</p>
+					</div>
+					<FeatureCard />
+				</div>
 
 				{/* Main goal */}
 				<div id="info" className="flex flex-col gap-4 mt-8">
@@ -79,18 +121,8 @@ export default function Home() {
 						className="w-full rounded-xl border-2 border-gray-200 p-1 shadow-lg"
 					/>
 				</div>
-				<div className="flex flex-col gap-4 mt-12">
-					<h2 className="md:text-2xl text-lg font-semibold">Features</h2>
-					<p>
-						Explore the various features of this image editor, powered by Rust
-						and WebAssembly.
-					</p>
-					<div className="mx-auto w-full grid sm:grid-cols-2 gap-4 items-center justify-center">
-						{features.map((feat, index) => (
-							<FeatureCard key={index} {...feat} />
-						))}
-					</div>
-				</div>
+
+				{/* Footer */}
 				<footer className="flex w-full justify-between mt-8 h-20">
 					<span className="text-sm">
 						Created by{" "}
--- components/feature-card.tsx
+++ components/feature-card.tsx
@@ -3,72 +3,119 @@ import { Badge } from "./ui/badge";
 import { Card, CardContent } from "./ui/card";
 import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "./ui/dialog";
 import Image from "next/image";
+import { useState } from "react";
 
-interface FeatureCardProps {
-	title: string;
-	demo?: string;
-	icon?: string;
-	textColor?: string;
-	backgroundColor?: string;
-}
+const features = [
+	{
+		title: "Adjust Color Image",
+		demo: "feature-demo/color.mp4",
+		icon: "feature-icon/four.svg",
+		backgroundColor: "bg-[#FFE792]",
+		zIndex: 10,
+		position: 0,
+	},
+	{
+		title: "Adjust Lightness",
+		demo: "feature-demo/light.mp4",
+		icon: "feature-icon/three.svg",
+		backgroundColor: "bg-[#ff8787]",
+		zIndex: 20,
+		position: 20,
+	},
+	{
+		title: "Adjust Sharpness",
+		demo: "feature-demo/sharp.mp4",
+		icon: "feature-icon/two.svg",
+		backgroundColor: "bg-[#69DB7C]",
+		zIndex: 30,
+		position: 40,
+	},
+	{
+		title: "Image Color Transfer",
+		demo: "feature-demo/transfer-color.mp4",
+		icon: "feature-icon/one.svg",
+		backgroundColor: "bg-[#6CBBFB]",
+		zIndex: 40,
+		position: 60,
+	},
+];
 
-export default function FeatureCard({
-	title,
-	demo,
-	icon,
-	backgroundColor,
-}: FeatureCardProps) {
+export default function FeatureCard() {
+	const [hoverIndex, setHoverIndex] = useState<number | null>(null);
 	return (
-		<Card className={`h-full w-full lg:min-h-80 ${backgroundColor}`}>
-			<CardContent className="h-full w-full relative px-6">
-				<div className="flex flex-col justify-between items-start space-y-4 lg:col-span-2 col-span-3">
-					<div className="flex flex-col gap-2">
-						<h3
-							className={`lg:text-5xl md:text-3xl text-xl font-semibold w-[96%] opacity-60`}>
-							{title}
-						</h3>
-						<Dialog>
-							<DialogTrigger asChild>
-								<Badge
-									className={`cursor-pointer rounded-full h-7 w-7 p-0 tabular-nums opacity-60`}>
-									<Eye />
-								</Badge>
-							</DialogTrigger>
-							<DialogContent className="min-w-3/4">
-								<DialogTitle className="text-2xl font-semibold">
-									{title}
-								</DialogTitle>
-								<div className="flex flex-col gap-4">
-									<video
-										src={demo}
-										autoPlay
-										loop
-										className="w-full rounded-lg border-2 border-gray-200 p-1 shadow-lg"
-									/>
-									<p className="text-sm text-muted-foreground">
-										This is a demo of the <strong>{title}</strong> feature.
-									</p>
+		<div className="mx-auto w-full flex lg:justify-end sm:justify-center justify-start items-center">
+			<div className="relative sm:h-96 h-56 sm:w-96 w-[80%] lg:mt-0 my-20">
+				{features.map((feat, index) => (
+					<Card
+						key={index}
+						style={{
+							position: "absolute",
+							top: `${
+								hoverIndex === index ? feat.position - 100 : feat.position
+							}px`,
+							left: `${feat.position}px`,
+							zIndex: `${feat.zIndex}`,
+							transition: "all 500ms ease-in-out",
+							cursor: "pointer",
+						}}
+						onMouseEnter={() => setHoverIndex(index)}
+						onMouseLeave={() => setHoverIndex(null)}
+						className={`h-full sm:w-96 w-[100%] max-h-80 ${feat.backgroundColor} -skew-y-6 shadow-2xl`}>
+						<CardContent className="h-full w-full relative px-6">
+							<div className="flex flex-col justify-between items-start space-y-4 lg:col-span-2 col-span-3">
+								<div className="flex flex-col gap-2">
+									<Dialog>
+										<DialogTrigger asChild>
+											<h3
+												className={`lg:text-5xl md:text-3xl text-xl font-semibold w-[96%] opacity-60`}>
+												{feat.title}
+											</h3>
+										</DialogTrigger>
+										<DialogTrigger asChild>
+											<Badge
+												className={`cursor-pointer rounded-full h-7 w-7 p-0 tabular-nums opacity-60`}>
+												<Eye />
+											</Badge>
+										</DialogTrigger>
+										<DialogContent className="min-w-3/4">
+											<DialogTitle className="text-2xl font-semibold">
+												{feat.title}
+											</DialogTitle>
+											<div className="flex flex-col gap-4">
+												<video
+													src={feat.demo}
+													autoPlay
+													loop
+													className="w-full rounded-lg border-2 border-gray-200 p-1 shadow-lg"
+												/>
+												<p className="text-sm text-muted-foreground">
+													This is a demo of the <strong>{feat.title}</strong>{" "}
+													feature.
+												</p>
+											</div>
+										</DialogContent>
+									</Dialog>
 								</div>
-							</DialogContent>
-						</Dialog>
-					</div>
-				</div>
-				<div
-					className="flex absolute sm:bottom-0 sm:right-0 -bottom-6 right-2 sm:w-auto sm:h-auto w-[100px] h-[100px] overflow-hidden"
-					style={{ pointerEvents: "none" }}>
-					<Image
-						src={icon || "globe.svg"}
-						alt={title}
-						width={100}
-						height={100}
-						className={`lg:w-28 ${
-							icon?.includes("four")
-								? "lg:h-36 sm:h-28 h-32"
-								: "lg:h-28 sm:h-22 h-28"
-						}`}
-					/>
-				</div>
-			</CardContent>
-		</Card>
+							</div>
+							<div
+								className="flex absolute sm:bottom-0 sm:right-0 -bottom-6 right-2 sm:w-auto sm:h-auto w-[100px] h-[100px] overflow-hidden"
+								style={{ pointerEvents: "none" }}>
+								<Image
+									src={feat.icon || "globe.svg"}
+									alt={feat.title}
+									width={100}
+									height={100}
+									className={`lg:w-28 ${
+										feat.icon?.includes("four")
+											? "lg:h-36 sm:h-28 h-32"
+											: "lg:h-28 sm:h-22 h-28"
+									}`}
+								/>
+							</div>
+						</CardContent>
+					</Card>
+				))}
+			</div>
+		</div>
 	);
 }
