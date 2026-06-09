7eee54f124de6d343de937d6cb9d6b655e5e935c
alfazh123 <alfazh291@gmail.com>
2026-06-09 16:42:08 +0700

    fix boilerplate image editor hook

--- app/hooks/useImageEditor.tsx
+++ app/hooks/useImageEditor.tsx
@@ -1,53 +1,27 @@
 // Create custom hooks file: hooks/useImageEditor.ts
-import { useState, useCallback } from 'react';
-import { ColorValueProps, LightValueProps } from '../wasm/type';
+import { useState, useCallback } from "react";
 
 export const useImageEditor = () => {
 	const [originalImgArr, setOriginalImgArr] = useState<Uint8Array>(
-		new Uint8Array()
+		new Uint8Array(),
 	);
 	const [editedImgArr, setEditedImgArr] = useState<Uint8Array>(
-		new Uint8Array()
+		new Uint8Array(),
 	);
+	const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
+
 	const [refImgArr, setRefImgArr] = useState<Uint8Array>(new Uint8Array());
 	const [imgUrl, setImgUrl] = useState<string | null>(null);
 	const [imgRefUrl, setImgRefUrl] = useState<string | null>(null);
-	const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
 	const [refSize, setRefSize] = useState({ width: 0, height: 0 });
 	const [isLoading, setIsLoading] = useState(false);
-
-	const [sharpVal, setSharpVal] = useState(0);
-	const [lightVal, setLightVal] = useState<LightValueProps>({
-		exposureValue: 0,
-		contrastValue: 0,
-	});
-	const [colorVal, setColorVal] = useState<ColorValueProps>({
-		saturationValue: 0,
-		temperatureValue: 0,
-		tintValue: 0,
-	});
 
 	const [windowSize, setWindowSize] = useState({ width: 0, height: 0 });
 	const [welcomeOverlay, setWelcomeOverlay] = useState(true);
 
-	const [zoomLevel, setZoomLevel] = useState(1);
-	const handleZoomReset = () => setZoomLevel(1);
-	const handleZoomChange = (value: number[]) => {
-		const newZoom = value[0] / 100; // Convert percentage to decimal
-		setZoomLevel(Math.max(0.5, Math.min(newZoom, 2))); // Clamp between 0.5 and 3
-	};
-
-	const handleOnWheel = (event: WheelEvent) => {
-		if (event.deltaY < 0) {
-			setZoomLevel(Math.max(0.5, Math.min(zoomLevel + 0.1, 2)));
-		} else if (event.deltaY > 0) {
-			setZoomLevel(Math.max(0.5, Math.min(zoomLevel - 0.1, 2)));
-		}
-	};
-
 	const ArrToURL = useCallback((arr: Uint8Array): string => {
 		return URL.createObjectURL(
-			new Blob([new Uint8Array(arr)], { type: "image/png" })
+			new Blob([new Uint8Array(arr)], { type: "image/png" }),
 		);
 	}, []);
 
@@ -69,22 +43,34 @@ export const useImageEditor = () => {
 		setImageSize,
 		isLoading,
 		setIsLoading,
-		sharpVal,
-		setSharpVal,
-		lightVal,
-		setLightVal,
-		colorVal,
-		setColorVal,
 		windowSize,
 		setWindowSize,
 		welcomeOverlay,
 		setWelcomeOverlay,
-		zoomLevel,
-		setZoomLevel,
-		handleZoomReset,
-		handleZoomChange,
-		handleOnWheel,
 		// Utils
 		ArrToURL,
 	};
 };
+
+export function useZoom() {
+	const [zoomLevel, setZoomLevel] = useState(1);
+	const handleZoomReset = () => setZoomLevel(1);
+	const handleZoomChange = (value: number[]) => {
+		const newZoom = value[0] / 100; // Convert percentage to decimal
+		setZoomLevel(Math.max(0.5, Math.min(newZoom, 2))); // Clamp between 0.5 and 3
+	};
+
+	const handleOnWheel = (event: WheelEvent) => {
+		if (event.deltaY < 0) {
+			setZoomLevel(Math.max(0.5, Math.min(zoomLevel + 0.1, 2)));
+		} else if (event.deltaY > 0) {
+			setZoomLevel(Math.max(0.5, Math.min(zoomLevel - 0.1, 2)));
+		}
+	};
+	return {
+		zoomLevel,
+		handleZoomReset,
+		handleZoomChange,
+		handleOnWheel,
+	};
+}
